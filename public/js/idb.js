92e24a304c47e7a56d7aa2c52f5188c1da4d0965
jeremy0anderson <projects@jeremyanderson.dev>
2022-05-26 05:55:55 -0600

    resubmission

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,3 +1,58 @@
 let db;
 
+////////////
+const req = indexedDB.open('budget-tracker', 1);
 
+req.onupgradeneeded=(e)=>{
+    const db = e.target.result;
+    db.createObjectStore('new_entry', { autoIncrement: true });
+};
+req.onsuccess=(event)=>{
+    mainDB = event.target.result;
+    if (navigator.onLine) {
+        uploadData();
+    }
+};
+req.onerror=(e)=>{
+    console.log(e.target.errorCode);
+}
+//////////////
+
+
+
+const saveRecord = (data) => {
+    let ts = mainDB.transaction('new_entry', 'readwrite');
+    const entryStore = ts.objectStore('new_entry');
+    entryStore.add(data);
+}
+
+const update = (data)=> {
+    let ts = mainDB.transaction('new_entry', 'readwrite');
+    const entryStore = ts.objectStore('new_entry');
+    entryStore.add(data);
+}
+
+const uploadData = () =>{
+    let ts = mainDB.transaction('new_entry', 'readwrite');
+    const entryStore = ts.objectStore('new_entry');
+
+    const allEntries = entryStore.getAll();
+    allEntries.onsuccess = () => {
+        if (allEntries.result.length) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(allEntries.result),
+            }).then((res) => res.json())
+                .then(() => {
+                    const ts = mainDB.transaction('new_entry', 'readwrite');
+                    const entryStore = ts.objectStore('new_entry');
+                    entryStore.clear();
+                    alert("Updates made while offline have been successfully updated");
+                }).catch((err) => {
+                console.log(err);
+            });
+        }
+    };
+}
+// if offline -- once back online data pushed and synced with db
+window.addEventListener('online', uploadData);
